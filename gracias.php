f04de97af1a942eba3cf8b561e8bcf31ea03645f
Rafael Ceballos <prograc@gmail.com>
2019-07-31 20:45:38 -0500

    eliminar comentarios de log
    eliminar comentarios de log

--- gracias.php
+++ gracias.php
@@ -29,9 +29,9 @@
 
 
         if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully: " . $sql . "<br>";
+            //echo "New record created successfully: " . $sql . "<br>";
         } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            //echo "Error: " . $sql . "<br>" . $conn->error;
         }  
       
         $conn->close();
