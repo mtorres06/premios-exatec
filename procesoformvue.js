40bd046ae3e90e8f54ff04fd23b42d2d1ecddc83
Rafael de Jesus Ceballos Palacios <prograc@gmail.com>
2019-08-01 14:54:42 -0500

    validaciones

--- procesoformvue.js
+++ procesoformvue.js
@@ -7,8 +7,84 @@ var procesoForm = new Vue({
 
         showNext: true,
         showPrev: false,
+
+        formularioValues: {
+            personaFisica: false,
+            personaMoral: false,
+            contactoNombre: '',
+            nominacionPostumaSi: false,
+            nominacionPostumaNo: false,
+            trayectoria:false,
+            merito:false,
+            nominadoNombre: '',
+            evidenciaFile:'',
+            cv:'',
+        }
+
     },
     methods: {
+        validarFormulario: function() {
+            if((!!this.formularioValues.personaFisica) === false 
+            && (!!this.formularioValues.personaMoral) === false ) {
+                   return {
+                       valid: false,
+                       message: 'Seleccione persona física o moral.'
+                   };
+            }
+            if((!!this.formularioValues.nominacionPostumaSi) === false 
+            && (!!this.formularioValues.nominacionPostumaNo) === false ) {
+                   return {
+                       valid: false,
+                       message: 'Seleccione si es o no una nominación póstuma.'
+                   };
+            }
+            if(!!this.formularioValues.nominadoNombre === false) {
+                return {
+                    valid: false,
+                    message: 'Ingrese el nombre del nominado.'
+                };
+            }
+            if(!!this.formularioValues.contactoNombre === false) {
+                return {
+                    valid: false,
+                    message: 'Ingrese el nombre del contacto.'
+                };
+            }
+            // if(!!this.formularioValues.evidenciaFile === false) {
+            //    return {
+              //      valid: false,
+                //    message: 'El archivo de evidencias es requerido.'
+          //      };
+            //}
+        //    if(!!this.formularioValues.cv === false) {
+         //       return {
+          //          valid: false,
+           //         message: 'El CV es requerido.'
+            //    };
+            //}
+            return {
+                valid: true,
+                message: ''
+            };
+        },
+        personaFisicaChecks: function() {
+            this.formularioValues.personaMoral = !this.formularioValues.personaFisica;
+        },
+        personaMoralChecks: function() {
+            this.formularioValues.personaFisica = !this.formularioValues.personaMoral;
+        },
+        nominacionPostumaSiChecks: function() {
+            this.formularioValues.nominacionPostumaNo = !this.formularioValues.nominacionPostumaSi;
+        },
+        nominacionPostumaNoChecks: function() {
+            this.formularioValues.nominacionPostumaSi = !this.formularioValues.nominacionPostumaNo;
+        },
+        trayectoriaChecks: function() {
+            this.formularioValues.merito = !this.formularioValues.trayectoria;
+        },
+        meritoChecks: function(){
+            this.formularioValues.trayectoria = !this.formularioValues.merito;
+        },
         goToStep1: function() {
             this.showStep1 = true;
             this.showStep2 = false;
@@ -54,11 +130,23 @@ var procesoForm = new Vue({
         enviar:function(event) {
             var formulario = $('#formulario').serialize();
             var url = $('#formulario')[0].action;
-            if(!formulario){
-                event.preventDefault();
+
+            var validado = this.validarFormulario();
+            if(validado.valid === false)
+            {
+                swal("Error de validación", validado.message, "info");
+                //event.preventDefault();
+                // return;
             }
-            else {
-                console.log(formulario);
+            else
+            {
+                if(!formulario){
+                    //event.preventDefault();
+                }
+                else {
+                    console.log(formulario);
+                    $('#formulario').submit();
+                }
             }
         },
         Init: function() {
